3ce181c626b1b289282a85297017ddca2c0f696b
Justin Rüdiger <ruedigju@noreply@infint.de>
2022-01-28 23:19:02 +0100

    „src/Prefabs/CardData.java“ ändern

--- src/Prefabs/CardData.java
+++ src/Prefabs/CardData.java
@@ -1,7 +1,7 @@
 package Prefabs;
 
 public class CardData {
-	public int cardNumber; // 1: Ass 2-10: Numbers 11:J 12:Q 13:Q
+	public int cardNumber; // 1: Ass 2-10: Numbers 11:J 12:Q 13:K
 	public int cardType; // 0: 1: 2: 3: TODO: PASCAL: Such dir aus welche ID welches Symbol ist
 	
 	public CardData(int number, int type) {
